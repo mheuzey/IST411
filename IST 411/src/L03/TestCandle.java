00e614e77e83c93bfe2de079d51ef2fe0e0e0de8
MICHAELLAPTOP\mikeh <michael.heuzey@gmail.com>
2018-02-11 20:59:34 -0500

    member variables, advice text

--- IST 411/src/L03/TestCandle.java
+++ IST 411/src/L03/TestCandle.java
@@ -19,23 +19,22 @@ import java.util.List;
 import javax.net.ssl.HttpsURLConnection;
 
 public class TestCandle extends JFrame {
-    private String ticker;
-    private int interval;
-    private String stockSymbol;
+    private String interval, stockSymbol;
     private ChartPanel chartPanel;
     private JPanel panel;
     private XYPlot mainPlot;
     private JFreeChart chart;
-    JLabel highest, lowest;
+    private JLabel lblHighest, lblLowest, lblAdvice;
     private final String address = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY";
-    String[] stock_list = {"AAPL", "MSFT", "GOOGL","INTC", "NVDA"};
-    String[] itv_list = {"1", "5", "15", "30", "60"};
+    private final String[] stock_list = {"AAPL", "AMZN", "DIS", "F", "GE", "GOOGL", "INTC", "MSFT", "NFLX", "NVDA", "TSLA"};
+    private final String[] itv_list = {"1min", "5min", "15min", "30min", "60min"};
+    private OHLCDataItem[] quotes;
     
-    public TestCandle(String stockSymbol, int interval) {
-        super("CandlestickDemo");
+    public TestCandle() {
+        super("Stock Data");
         
-        this.stockSymbol = stockSymbol;
-        this.interval = interval;
+        this.stockSymbol = stock_list[0];
+        this.interval = itv_list[0];
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -44,7 +43,7 @@ public class TestCandle extends JFrame {
         DateAxis    domainAxis       = new DateAxis("Date");
         NumberAxis  rangeAxis        = new NumberAxis("Price");
         CandlestickRenderer renderer = new CandlestickRenderer();
-        XYDataset   dataset          = getDataSet(stockSymbol, interval);
+        XYDataset   dataset          = getDataSet();
 
         mainPlot = new XYPlot(dataset, domainAxis, rangeAxis, renderer);
 
@@ -53,41 +52,54 @@ public class TestCandle extends JFrame {
         rangeAxis.setAutoRangeIncludesZero(false);
         domainAxis.setTimeline( SegmentedTimeline.newMondayThroughFridayTimeline() );
 
-        
         chart = new JFreeChart(stockSymbol, null, mainPlot, false);
         chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(600, 300));
+        chartPanel.setPreferredSize(new Dimension(1200, 600));
         
         JPanel infoPanel = new JPanel();
         JPanel selectPanel = new JPanel();
         
-        highest = new JLabel("Highest: " + getHighest(stockSymbol));
-        lowest = new JLabel("Lowest: " + getLowest(stockSymbol));
-        
-        infoPanel.add(highest);
-        infoPanel.add(lowest);
+        OHLCDataItem high = getHighest(), low = getLowest();
+        String advice = "";
+        if (low.getDate().after(high.getDate())) {
+            advice = "If you shorted at " + high.getHigh() + " and bought back at " + low.getLow();
+        } else {
+            advice = "If you bought at " + low.getLow() + " and sold at " + high.getHigh();
+        }
+        advice += ", you would have made ";
+        double result = (high.getHigh().doubleValue() / low.getLow().doubleValue() - 1) * 100;
+        advice += String.format("%.2f%s", result, "%.");
+        lblAdvice = new JLabel(advice);
+        infoPanel.add(lblAdvice);
         
         JLabel stockLabel = new JLabel("Stock: ");
         JLabel intervalLabel = new JLabel("Interval: ");
         JComboBox stockCB = new JComboBox(stock_list);
         JComboBox intervalCB = new JComboBox(itv_list);
         JButton refreshButton = new JButton("Refresh");
-        refreshButton.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                String selected_stock = (String)stockCB.getSelectedItem();
-                int selected_interval = Integer.parseInt((String)intervalCB.getSelectedItem());
+        refreshButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                stockSymbol = (String)stockCB.getSelectedItem();
+                interval = (String)intervalCB.getSelectedItem();
                 
-                mainPlot = new XYPlot(getDataSet(selected_stock, selected_interval), domainAxis, rangeAxis, renderer);
-                chart = new JFreeChart(selected_stock, null, mainPlot, false);
+                mainPlot = new XYPlot(getDataSet(), domainAxis, rangeAxis, renderer);
+                chart = new JFreeChart(stockSymbol, null, mainPlot, false);
                 
                 chartPanel.setChart(chart);
                 chartPanel.validate();
                 chartPanel.repaint();
                 
-                highest.setText("Highest: " + getHighest(selected_stock));
-                lowest.setText("Lowest: " + getLowest(selected_stock));
+                OHLCDataItem high = getHighest(), low = getLowest();
+                String advice = "";
+                if (low.getDate().after(high.getDate())) {
+                    advice = "If you shorted at " + high.getHigh() + " and bought back at " + low.getLow();
+                } else {
+                    advice = "If you bought at " + low.getLow() + " and sold at " + high.getHigh();
+                }
+                advice += ", you would have made ";
+                double result = (high.getHigh().doubleValue() / low.getLow().doubleValue() - 1) * 100;
+                advice += String.format("%.2f%s", result, "%.");
+                lblAdvice.setText(advice);
                 
                 panel.validate();
                 panel.repaint();
@@ -107,47 +119,35 @@ public class TestCandle extends JFrame {
         this.add(panel);
         this.pack();
     }
-    protected AbstractXYDataset getDataSet(String stockSymbol, int interval) {
-        //This is the dataset
-        DefaultOHLCDataset result = null;
-        //This is the data needed
-        OHLCDataItem[] data;
-        
-        data = getData(stockSymbol, interval);
-
-        //Create an Open, High, Low, Close dataset
-        result = new DefaultOHLCDataset(stockSymbol, data);
-
-        return result;
+    
+    protected AbstractXYDataset getDataSet() {
+        getData();
+        return new DefaultOHLCDataset(stockSymbol, quotes);
     }
    
-    protected OHLCDataItem[] getData(String stockSymbol, int interval) {
+    protected void getData() {
         List<OHLCDataItem> dataItems = new ArrayList<OHLCDataItem>();
 
         try {
-            //URL url = new URL(address + "&symbol=" + ticker + "&interval=" + interval + "&apikey=EK35BIPXHQP01Z6W" + "&datatype=csv");
-            String strUrl= "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockSymbol + "&interval=" + interval + "min&apikey=42LP66DV8I8H4X64&datatype=csv";
+            String strUrl = address + "&symbol=" + stockSymbol + "&interval=" + interval + "&apikey=42LP66DV8I8H4X64&datatype=csv";
             URL url = new URL(strUrl);
-            HttpsURLConnection is= (HttpsURLConnection) url.openConnection();
+            HttpsURLConnection is = (HttpsURLConnection) url.openConnection();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is.getInputStream()));
             
             DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
             String inputLine;
-            rd.readLine();
+            rd.readLine();      // skip header line
             while ((inputLine = rd.readLine()) != null) {
                 StringTokenizer st = new StringTokenizer(inputLine, ",");
 
-                Date timestamp       = df.parse( st.nextToken() );
-                System.out.println(timestamp);
+                Date timestamp  = df.parse( st.nextToken() );
                 double open     = Double.parseDouble( st.nextToken() );
-                System.out.println(open);
                 double high     = Double.parseDouble( st.nextToken() );
                 double low      = Double.parseDouble( st.nextToken() );
                 double close    = Double.parseDouble( st.nextToken() );
                 double volume   = Double.parseDouble( st.nextToken() );
                 
-
                 OHLCDataItem item = new OHLCDataItem(timestamp, open, high, low, close, volume);
                 dataItems.add(item);
             }
@@ -157,89 +157,31 @@ public class TestCandle extends JFrame {
             e.printStackTrace();
         }
         
-
         //Convert the list into an array
-        OHLCDataItem[] data = dataItems.toArray(new OHLCDataItem[dataItems.size()]);
-
-        return data;
+        quotes =  dataItems.toArray(new OHLCDataItem[dataItems.size()]);
     }
     
-    protected Double getHighest(String stockSymbol){
-        List<Double> highs = new ArrayList<Double>();
-        
-                try {
-            //URL url = new URL(address + "&symbol=" + ticker + "&interval=" + interval + "&apikey=EK35BIPXHQP01Z6W" + "&datatype=csv");
-            String strUrl= "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockSymbol + "&interval=60min&apikey=42LP66DV8I8H4X64&datatype=csv";
-            URL url = new URL(strUrl);
-            HttpsURLConnection is= (HttpsURLConnection) url.openConnection();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is.getInputStream()));
-            
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
-            String inputLine;
-            rd.readLine();
-            while ((inputLine = rd.readLine()) != null) {
-                StringTokenizer st = new StringTokenizer(inputLine, ",");
-
-                Date timestamp       = df.parse( st.nextToken() );
-                double open     = Double.parseDouble( st.nextToken() );
-                double high     = Double.parseDouble( st.nextToken() );
-                highs.add(high);
-                double low      = Double.parseDouble( st.nextToken() );
-                double close    = Double.parseDouble( st.nextToken() );
-                double volume   = Double.parseDouble( st.nextToken() );
-                
-
-                OHLCDataItem item = new OHLCDataItem(timestamp, open, high, low, close, volume);
+    protected OHLCDataItem getHighest(){
+        OHLCDataItem ret = quotes[0];
+        for (int i = 0; i < quotes.length; i++) {
+            if (quotes[i].getHigh().doubleValue() > ret.getHigh().doubleValue()) {
+                ret = quotes[i];
             }
-            rd.close();
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-                
-        return Collections.max(highs);
+        return ret;
     }
     
-        protected Double getLowest(String stockSymbol){
-        List<Double> lows = new ArrayList<Double>();
-        
-                try {
-            //URL url = new URL(address + "&symbol=" + ticker + "&interval=" + interval + "&apikey=EK35BIPXHQP01Z6W" + "&datatype=csv");
-            String strUrl= "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockSymbol + "&interval=60min&apikey=42LP66DV8I8H4X64&datatype=csv";
-            URL url = new URL(strUrl);
-            HttpsURLConnection is= (HttpsURLConnection) url.openConnection();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is.getInputStream()));
-            
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
-            String inputLine;
-            rd.readLine();
-            while ((inputLine = rd.readLine()) != null) {
-                StringTokenizer st = new StringTokenizer(inputLine, ",");
-
-                Date timestamp       = df.parse( st.nextToken() );
-                double open     = Double.parseDouble( st.nextToken() );
-                double high     = Double.parseDouble( st.nextToken() );
-                double low      = Double.parseDouble( st.nextToken() );
-                lows.add(low);
-                double close    = Double.parseDouble( st.nextToken() );
-                double volume   = Double.parseDouble( st.nextToken() );
-                
-
-                OHLCDataItem item = new OHLCDataItem(timestamp, open, high, low, close, volume);
+    protected OHLCDataItem getLowest(){
+        OHLCDataItem ret = quotes[0];
+        for (int i = 0; i < quotes.length; i++) {
+            if (quotes[i].getLow().doubleValue() < ret.getLow().doubleValue()) {
+                ret = quotes[i];
             }
-            rd.close();
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-                
-        return Collections.min(lows);
+        return ret;
     }
-   
     
     public static void main(String[] args) {
-        new TestCandle("SRCE", 60).setVisible(true);
+        new TestCandle().setVisible(true);
     }
 }
